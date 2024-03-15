97ec9c6044e54c2aeaf51cd5f2b40191dae20877
Leilani <pquaby@gmail.com>
2024-03-15 19:03:00 -0400

    Add logic to turn counter red when it goes below the limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-const textBox = $("textarea.tweet-box");
 
-  
+  const textBox = $("textarea.tweet-box");
+
   textBox.on('input', function() {
     //Get the input length in the textarea
     const length = $(this).val().length;
@@ -9,9 +9,21 @@ const textBox = $("textarea.tweet-box");
     const form = $(this).closest("form");
     const output = form.find("output.counter");
 
+    //Show the remaining available characters in the counter
     output.text(140 - length);
+    
 
-  })
+    //Change the counter to red if it goes below the max allowed
+    let remainingChars = 140 - length;
 
+    if (remainingChars < 0) {
+      $("output.counter").addClass("red-counter");
+    }
+
+    if (remainingChars > 0) {
+      $("output.counter").removeClass("red-counter");
+    }
+
+  });
 
 });

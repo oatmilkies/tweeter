07b8740fd33c643d61fbe9386cda6554795e0058
Leilani <pquaby@gmail.com>
2024-03-20 14:09:00 -0400

    Remove hard coded tweets and add ajax GET to render tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,6 @@
 
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   const createTweetElement = function(tweetData) {
     //Create the tweet using the same format as in index.html
     const markup = `
@@ -61,14 +34,12 @@ $(document).ready(function() {
     //Append each tweet
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('.tweet-container').append($tweet);
+      $('.tweet-container').prepend($tweet);
     }
 
   };
 
-  renderTweets(data);
-
-
+  //Post a new tweet
   $("#tweet-form").on("submit", function(event) {
     event.preventDefault();
 
@@ -88,8 +59,16 @@ $(document).ready(function() {
         console.error("Couldn't submit tweet");
       }
     });
-
     
-  })
+  });
+
+  //Load past tweets
+  const loadTweets = function() {
+    $.ajax("/tweets", "initial-tweets.json", { method: "GET"})
+    .then(initialTweets => renderTweets(initialTweets))
+    .catch(err => console.log(err))
+  };
+
+  loadTweets();
 
 });
